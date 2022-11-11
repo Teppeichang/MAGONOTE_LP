756d857c38d97edd8fc2e9488e0baea2dd985e7f
Teppei Torii <teppeichan9@gmail.com>
2022-11-11 16:38:46 +0900

    Add: ページファイル(portfolio,contact,pricing,trial)

---
+++ pages/contact.jsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <>
+      <h1>This is Contact(お問い合わせフォーム) Page</h1>
+    </>
+  )
+}
+
+export default Contact
---
+++ pages/trial.jsx
@@ -0,0 +1,9 @@
+const Trial = () => {
+  return (
+    <>
+      <h1>This is Trial(トライアル申込フォーム) Page</h1>
+    </>
+  )
+}
+
+export default Trial
