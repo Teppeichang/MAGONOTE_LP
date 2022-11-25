70495b33468c473d0d89e6d4d67d8d08373b5679
Teppei Torii <teppeichan9@gmail.com>
2022-11-25 15:49:40 +0900

    Feat: パンくずリストの実装

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,10 +1,12 @@
 import Header from "./header";
 import Footer from "./footer";
+import BreadCrumb from "../components/breadCrumb"
 
 const Layout = (props) => {
   return (
     <>
       <Header />
+      <BreadCrumb />
       {props.children}
       <Footer />
     </>
