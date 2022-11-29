82138f8c81b75d99d86e1ab06ee6d46f1bb2952f
Teppei Torii <teppeichan9@gmail.com>
2022-11-29 16:30:44 +0900

    Fix: Input部分をMUIからTailwindベースのUIに差し替え

--- pages/trial.jsx
+++ pages/trial.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import Layout from "../components/layout";
-import { TextField } from "@mui/material";
-import Checkbox from "@mui/material/Checkbox";
 import Image from "next/image";
-import BreadCrumb from "../components/breadCrumb"
+import BreadCrumb from "../components/breadCrumb";
 
 const Trial = () => {
-  const lists = [{string: "トップページ", path: "/"}, {string: "無料で始める", path: "/trial"}]
+  const lists = [
+    { string: "トップページ", path: "/" },
+    { string: "無料で始める", path: "/trial" },
+  ];
   const [checked, setChecked] = useState(false);
   const handleChange = (event) => {
     setChecked(event.target.checked);
@@ -15,11 +16,16 @@ const Trial = () => {
   return (
     <Layout>
       <BreadCrumb lists={lists} />
-      <div className="flex flex-row justify-center items-center bg-yellow-400 m-20">
-        <div className="bg-white py-12 px-20">
+      <div className="flex flex-row justify-center items-center bg-yellow-400 m-20 p-10">
+        <div className="bg-white py-10 px-10">
           <p>MAGONOTE</p>
           <div>
-            <Image src="/images/sincerely-media-xJlc_Ke3oNI-unsplash.jpg" width={480} height={400} quality={90}/>
+            <Image
+              src="/images/sincerely-media-xJlc_Ke3oNI-unsplash.jpg"
+              width={480}
+              height={400}
+              quality={90}
+            />
           </div>
           <ul className="list-disc list-inside">
             <li>クレジットカード登録不要</li>
@@ -34,19 +40,49 @@ const Trial = () => {
           </div>
           <form className="m-10">
             <div className="flex flex-row">
-              <TextField required id="outlined-required" label="姓" sx={{ my: 1 }} />
-              <TextField required id="outlined-required" label="名" sx={{ my: 1 }} />
+              <input
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block p-2.5 m-2"
+                placeholder="姓"
+                required
+              />
+              <input
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block p-2.5 m-2"
+                placeholder="名"
+                required
+              />
             </div>
             <div className="flex flex-col">
-              <TextField required id="outlined-required" label="会社名" sx={{ my: 1 }} />
-              <TextField required id="outlined-required" label="メールアドレス" sx={{ my: 1 }} />
-              <TextField required id="outlined-required" label="電話番号" sx={{ my: 1 }} />
+              <input
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block p-2.5 m-2"
+                placeholder="会社名"
+                required
+              />
+              <input
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block p-2.5 m-2"
+                placeholder="メールアドレス"
+                required
+              />
+              <input
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block p-2.5 m-2"
+                placeholder="電話番号"
+                required
+              />
             </div>
             <div className="flex flex-row items-center">
-              <p>利用規約に同意する</p>
-              <Checkbox checked={checked} onChange={handleChange} color="default" />
+              <p>利用規約に同意する*</p>
+              <input
+                required
+                type="checkbox"
+                value=""
+                class="w-4 h-4 text-black bg-gray-100 rounded border-gray-300 "
+              />
             </div>
-            <button class="bg-black hover:bg-slate-700 text-white font-medium py-2 px-4 rounded" type="submit">今すぐ無料で始める</button>
+            <button
+              className="bg-black hover:bg-slate-700 text-white font-medium py-2 px-4 rounded"
+              type="submit"
+            >
+              今すぐ無料で始める
+            </button>
           </form>
         </div>
       </div>
