159f32b43cfde84e654cb85fd1cdd73ad30cbfa6
Teppei Torii <teppeichan9@gmail.com>
2022-12-27 10:14:52 +0900

    Fix: 画像の配置レイアウト

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -1,5 +1,4 @@
 import Layout from "../components/layout";
-import Link from "next/link";
 import Image from "next/image";
 import BreadCrumb from "../components/breadCrumb";
 
@@ -18,47 +17,55 @@ const Portfolio = () => {
             アナリティクス、SEO、ウェブ広告、ソーシャルメディアなどのレポートテンプレートを豊富にご用意しています。テンプレートをそのまま利用することも、カスタマイズして利用することも両方可能です。
           </p>
         </div>
-        <div className="flex justify-between mt-10 mx-20">
-          <div className="flex flex-col">
-            <Image
-              src="/images/c63232d34a5f72e41f735c9cb45ec181.png"
-              width={560}
-              height={720}
-              quality={90}
-            />
-            <Link href={"/portfolio/facebook-report"}>Facebook広告レポート</Link>
-          </div>
-          <div className="flex flex-col">
-            <Image
-              src="/images/4da13f27211a8ffbdd9d1ff88f877952.png"
-              alt="LINE広告レポート"
-              width={560}
-              height={720}
-              quality={90}
-            />
-            <Link href={"/portfolio/line-report"}>LINE広告レポート</Link>
-          </div>
-        </div>
-        <div className="flex justify-between mt-10 mx-20">
-          <div className="flex flex-col">
-            <Image
-              src="/images/5dd2e69e7ca524605f094ecb720bd83e.png"
-              alt="TikTok広告レポート"
-              width={560}
-              height={720}
-              quality={90}
-            />
-            <Link href={"/portfolio/tiktok-report"}>TikTok広告レポート</Link>
-          </div>
-          <div className="flex flex-col">
-            <Image
-              src="/images/db9717065f66815dee7c256ed7cd2705.png"
-              alt="Google & YouTube広告レポート"
-              width={560}
-              height={720}
-              quality={90}
-            />
-            <Link href={"/portfolio/google-and-youtube-report"}>Google&YouTube広告レポート</Link>
+        <div className="mt-10 mx-20">
+          <div className="flex flex-col lg:flex-row lg:flex-wrap justify-center items-center">
+            <div className="flex flex-col">
+              <a href={"/portfolio/facebook-report"}>
+                <Image
+                  src="/images/c63232d34a5f72e41f735c9cb45ec181.png"
+                  width={560}
+                  height={720}
+                  quality={90}
+                />
+              </a>
+              <a href={"/portfolio/facebook-report"}>Facebook広告レポート</a>
+            </div>
+            <div className="flex flex-col">
+              <a href={"/portfolio/line-report"}>
+                <Image
+                  src="/images/4da13f27211a8ffbdd9d1ff88f877952.png"
+                  alt="LINE広告レポート"
+                  width={560}
+                  height={720}
+                  quality={90}
+                />
+              </a>
+              <a href={"/portfolio/line-report"}>LINE広告レポート</a>
+            </div>
+            <div className="flex flex-col">
+              <a href={"/portfolio/tiktok-report"}>
+                <Image
+                  src="/images/5dd2e69e7ca524605f094ecb720bd83e.png"
+                  alt="TikTok広告レポート"
+                  width={560}
+                  height={720}
+                  quality={90}
+                />
+              </a>
+              <a href={"/portfolio/tiktok-report"}>TikTok広告レポート</a>
+            </div>
+            <div className="flex flex-col">
+              <a href={"/portfolio/google-and-youtube-report"}>
+                <Image
+                  src="/images/db9717065f66815dee7c256ed7cd2705.png"
+                  alt="Google & YouTube広告レポート"
+                  width={560}
+                  height={720}
+                  quality={90}
+                />
+              </a>
+              <a href={"/portfolio/google-and-youtube-report"}>Google&YouTube広告レポート</a>
+            </div>
           </div>
         </div>
       </div>
