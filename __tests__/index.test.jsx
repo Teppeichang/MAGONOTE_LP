60c13eb1c1e3e14e2b21a0dafe1e47b9df2fbbfc
Teppei Torii <teppeichan9@gmail.com>
2023-01-10 14:20:51 +0900

    Add: Indexに配置したリンク付きボタンのページ遷移に関する単体テストコード追加

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,12 +1,23 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
 import '@testing-library/jest-dom/extend-expect';
-import Layout from '../components/layout';
 import Index from '../pages/index';
+import Trial from "../pages/trial";
+import Portfolio from "../pages/portfolio";
 
-describe('トップページ(index.jsx)のレンダリングテスト', () => {
-  it('アクセス解析・広告・SNSなどすべてのレポートに対応 のテキストが表示されていること', () => {
-    render(<Index />);
-    expect(render(<Layout />))
-    expect(screen.getByText('アクセス解析・広告・SNSなどすべてのレポートに対応')).toBeInTheDocument();
-  })
+describe('Indexに配置したリンク付きボタンのページ遷移テスト', () => {
+  it('「今すぐ無料で始める」「無料で始める」ボタンのクリックで無料トライアルページへの遷移が正常に行われる', () => {
+    render(<Index />)
+    const trialLinkList = screen.getAllByText(/無料で始める/);
+    trialLinkList.forEach( (link) => {
+      userEvent.click(link)
+      expect(render(<Trial />))
+    });
+  });
+  it('「すべてのレポートを見る」ボタンのクリックでレポートギャラリーページへの遷移が正常に行われる', () => {
+    render(<Index />)
+    const portfolioLink = screen.getByText("すべてのレポートを見る");
+    userEvent.click(portfolioLink)
+    expect(render(<Portfolio />))
+  });
 })
