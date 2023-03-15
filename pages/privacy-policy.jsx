4b116f19bd4d7b9c33cc304c0e52f7f122636f69
Teppeichang <teppeichan9@gmail.com>
2023-03-15 17:33:56 +0900

    文字間隔調整(プライバシーポリシーページ、利用規約ページ PC・SP)

--- pages/privacy-policy.jsx
+++ pages/privacy-policy.jsx
@@ -11,28 +11,28 @@ const PrivacyPolicy = () => {
       <BreadCrumb lists={lists} />
       <div className="min-h-screen px-10 lg:px-20">
         <div className="my-10">
-          <h2 className="text-2xl font-bold mb-2">プライバシーポリシー</h2>
-          <p>
+          <p className="text-2xl font-bold mb-2 tracking-wide lg:tracking-wider">プライバシーポリシー</p>
+          <p className="tracking-wide lg:tracking-wider">
             株式会社Next
             Stage（以下、「当社」といいます。）は、本ウェブサイト上で提供するサービス（以下、「本サービス」といいます。）におけるユーザーの個人情報の取扱いについて、以下のとおりプライバシーポリシー（以下、「本ポリシー」といいます。）を定めます。
           </p>
         </div>
         <div className="mb-10">
-          <h2 className="text-2xl font-bold mb-2">第1条（個人情報）</h2>
-          <p>
+          <p className="text-2xl font-bold mb-2 tracking-wide lg:tracking-wider">第1条（個人情報）</p>
+          <p className="tracking-wide lg:tracking-wider">
             「個人情報」とは、個人情報保護法にいう「個人情報」を指すものとし、生存する個人に関する情報であって当該情報に含まれる氏名、生年月日、住所、電話番号、連絡先その他の記述等により特定の個人を識別できる情報及び容貌、指紋、声紋にかかるデータ、及び健康保険証の保険者番号などの当該情報単体から特定の個人を識別できる情報（個人識別情報）を指します。
           </p>
         </div>
         <div className="mb-10">
-          <h2 className="text-2xl font-bold mb-2">第2条（個人情報の収集方法）</h2>
-          <p>
+          <p className="text-2xl font-bold mb-2 tracking-wide lg:tracking-wider">第2条（個人情報の収集方法）</p>
+          <p className="tracking-wide lg:tracking-wider">
             当社は、ユーザーが利用登録をする際に氏名、生年月日、住所、電話番号、メールアドレス、銀行口座番号、クレジットカード番号、運転免許証番号などの個人情報をお尋ねすることがあります。また、ユーザーと提携先などとの間でなされたユーザーの個人情報を含む取引記録や決済に関する情報を,当社の提携先（情報提供元、広告主、広告配信先などを含みます。以下、｢提携先｣といいます。）などから収集することがあります。
           </p>
         </div>
         <div className="mb-10">
-          <h2 className="text-2xl font-bold mb-2">第3条（個人情報を収集・利用する目的）</h2>
-          <p className="mb-2">当社が個人情報を収集・利用する目的は、以下のとおりです。</p>
-          <ul className="list-disc list-inside ml-7">
+          <p className="text-2xl font-bold mb-2 tracking-wide lg:tracking-wider">第3条（個人情報を収集・利用する目的）</p>
+          <p className="mb-2 tracking-wide lg:tracking-wider">当社が個人情報を収集・利用する目的は、以下のとおりです。</p>
+          <ul className="list-disc list-inside ml-7 tracking-wide lg:tracking-wider">
             <li className="-indent-6">当社サービスの提供・運営のため</li>
             <li className="-indent-6">ユーザーからのお問い合わせに回答するため（本人確認を行うことを含む）</li>
             <li className="-indent-6">
@@ -50,8 +50,8 @@ const PrivacyPolicy = () => {
           </ul>
         </div>
         <div className="mb-10">
-          <h2 className="text-2xl font-bold mb-2">第4条（利用目的の変更）</h2>
-          <ul className="list-disc list-inside ml-7">
+          <p className="text-2xl font-bold mb-2 tracking-wide lg:tracking-wider">第4条（利用目的の変更）</p>
+          <ul className="list-disc list-inside ml-7 tracking-wide lg:tracking-wider">
             <li className="-indent-6">
               当社は、利用目的が変更前と関連性を有すると合理的に認められる場合に限り、個人情報の利用目的を変更するものとします。
             </li>
@@ -61,12 +61,12 @@ const PrivacyPolicy = () => {
           </ul>
         </div>
         <div className="mb-10">
-          <h2 className="text-2xl font-bold mb-2">第5条（個人情報の第三者提供）</h2>
-          <ul className="list-disc list-inside ml-7">
+          <p className="text-2xl font-bold mb-2 tracking-wide lg:tracking-wider">第5条（個人情報の第三者提供）</p>
+          <ul className="list-disc list-inside ml-7 tracking-wide lg:tracking-wider">
             <li className="-indent-6">
               当社は、次に掲げる場合を除いてあらかじめユーザーの同意を得ることなく、第三者に個人情報を提供することはありません。ただし、個人情報保護法その他の法令で認められる場合を除きます。
             </li>
-            <ul className="list-disc list-inside ml-7">
+            <ul className="list-disc list-inside ml-7 tracking-wide lg:tracking-wider">
               <li className="-indent-6">
                 人の生命、身体または財産の保護のために必要がある場合であって、本人の同意を得ることが困難であるとき
               </li>
@@ -80,7 +80,7 @@ const PrivacyPolicy = () => {
                 予め次の事項を告知あるいは公表し、かつ当社が個人情報保護委員会に届出をしたとき利用目的に第三者への提供を含むこと
               </li>
               <li className="-indent-6">第三者に提供されるデータの項目</li>
-              <ul className="list-disc list-inside ml-7">
+              <ul className="list-disc list-inside ml-7 tracking-wide lg:tracking-wider">
                 <li className="-indent-6">第三者への提供の手段または方法</li >
                 <li className="-indent-6">本人の求めに応じて個人情報の第三者への提供を停止すること</li>
                 <li className="-indent-6">本人の求めを受け付ける方法</li>
@@ -89,7 +89,7 @@ const PrivacyPolicy = () => {
             <li className="-indent-6">
               前項の定めにかかわらず、次に掲げる場合には当該情報の提供先は第三者に該当しないものとします。
             </li>
-            <ul className="list-disc list-inside ml-7">
+            <ul className="list-disc list-inside ml-7 tracking-wide lg:tracking-wider">
               <li className="-indent-6">
                 当社が利用目的の達成に必要な範囲内において個人情報の取扱いの全部または一部を委託する場合
               </li>
@@ -101,13 +101,13 @@ const PrivacyPolicy = () => {
           </ul>
         </div>
         <div className="mb-10">
-          <h2 className="text-2xl font-bold mb-2">第6条（個人情報の開示）</h2>
-          <ul className="list-disc list-inside ml-7">
+          <p className="text-2xl font-bold mb-2 tracking-wide lg:tracking-wider">第6条（個人情報の開示）</p>
+          <ul className="list-disc list-inside ml-7 tracking-wide lg:tracking-wider">
             <li className="-indent-6">
               当社は、本人から個人情報の開示を求められたときは本人に対し、遅滞なくこれを開示します。ただし、開示することにより次のいずれかに該当する場合は、その全部または一部を開示しないこともあり、開示しない決定をした場合にはその旨を遅滞なく通知します。なお、個人情報の開示に際しては、1件あたり1,000円の手数料を申し受けます。
             </li>
             <li className="-indent-6">本人または第三者の生命、身体、財産その他の権利利益を害するおそれがある場合</li>
-            <ul className="list-disc list-inside ml-7">
+            <ul className="list-disc list-inside ml-7 tracking-wide lg:tracking-wider">
               <li className="-indent-6">当社の業務の適正な実施に著しい支障を及ぼすおそれがある場合</li>
               <li className="-indent-6">その他法令に違反することとなる場合</li>
               <li className="-indent-6">
@@ -117,8 +117,8 @@ const PrivacyPolicy = () => {
           </ul>
         </div>
         <div className="mb-10">
-          <h2 className="text-2xl font-bold mb-2">第7条（個人情報の訂正および削除）</h2>
-          <ul className="list-disc list-inside ml-7">
+          <p className="text-2xl font-bold mb-2 tracking-wide lg:tracking-wider">第7条（個人情報の訂正および削除）</p>
+          <ul className="list-disc list-inside ml-7 tracking-wide lg:tracking-wider">
             <li className="-indent-6">
               ユーザーは、当社の保有する自己の個人情報が誤った情報である場合には当社が定める手続きにより、当社に対して個人情報の訂正、追加または削除（以下、「訂正等」といいます。）を請求することができます。
             </li>
@@ -131,8 +131,8 @@ const PrivacyPolicy = () => {
           </ul>
         </div>
         <div className="mb-10">
-          <h2 className="text-2xl font-bold mb-2">第8条（個人情報の利用停止等）</h2>
-          <ul className="list-disc list-inside ml-7">
+          <p className="text-2xl font-bold mb-2 tracking-wide lg:tracking-wider">第8条（個人情報の利用停止等）</p>
+          <ul className="list-disc list-inside ml-7 tracking-wide lg:tracking-wider">
             <li className="-indent-6">
               当社は、本人から個人情報が利用目的の範囲を超えて取り扱われているという理由、または不正の手段により取得されたものであるという理由により、その利用の停止または消去（以下、「利用停止等」といいます。）を求められた場合には、遅滞なく必要な調査を行います。
             </li>
@@ -148,8 +148,8 @@ const PrivacyPolicy = () => {
           </ul>
         </div>
         <div className="mb-10">
-          <h2 className="text-2xl font-bold mb-2">第9条（プライバシーポリシーの変更）</h2>
-          <ul className="list-disc list-inside ml-7">
+          <p className="text-2xl font-bold mb-2 tracking-wide lg:tracking-wider">第9条（プライバシーポリシーの変更）</p>
+          <ul className="list-disc list-inside ml-7 tracking-wide lg:tracking-wider">
             <li className="-indent-6">
               本ポリシーの内容は、法令その他本ポリシーに別段の定めのある事項を除いて、ユーザーに通知することなく変更することができるものとします。
             </li>
@@ -159,11 +159,11 @@ const PrivacyPolicy = () => {
           </ul>
         </div>
         <div className="mb-10">
-          <h2 className="text-2xl font-bold mb-2">第10条（お問い合わせ窓口）</h2>
-          <p>本ポリシーに関するお問い合わせは、下記の窓口までお願いいたします。</p>
-          <p>住所：〒114-0003 東京都北区豊島1-15-3 セントラル王子901</p>
-          <p>社名：株式会社Next Stage</p>
-          <p>メールアドレス：info@next-stage.biz</p>
+          <p className="text-2xl font-bold mb-2 tracking-wide lg:tracking-wider">第10条（お問い合わせ窓口）</p>
+          <p className="tracking-wide lg:tracking-wider">本ポリシーに関するお問い合わせは、下記の窓口までお願いいたします。</p>
+          <p className="tracking-wide lg:tracking-wider">住所：〒114-0003 東京都北区豊島1-15-3 セントラル王子901</p>
+          <p className="tracking-wide lg:tracking-wider">社名：株式会社Next Stage</p>
+          <p className="tracking-wide lg:tracking-wider">メールアドレス：info@next-stage.biz</p>
         </div>
       </div>
     </Layout>
