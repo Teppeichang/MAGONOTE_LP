c3036a21691a18a25c340a7767b983b2368494a7
Teppeichang <teppeichan9@gmail.com>
2023-03-31 14:35:04 +0900

    デザイン追加Fix: TOPページの紫色背景部分の不要な余白削除

--- pages/index.jsx
+++ pages/index.jsx
@@ -271,7 +271,7 @@ const Index = () => {
       </div>
       <Reveal animation={Animation.FadeIn} delay={500}>
         <div className="flex flex-col items-center bg-gradient-to-r from-purple-800 to-purple-600">
-          <p className="font-mPlus2c font-bold text-center text-2xl text-white mt-14 mx-4 tracking-wide lg:tracking-wider lg:mt-20 lg:mx-0">
+          <p className="font-mPlus2c font-bold text-center text-2xl text-white mt-14 tracking-wide lg:tracking-wider lg:mt-20 lg:mx-0">
             今すぐ無料でレポートを自動化し、改善アクションに時間を使おう
           </p>
           <Link href={"/trial"}>
